8695f7ccabab35b1dcb24d6eaca3b1fb5fb0388b
ryan-hughes97 <ryan.hughes.jp@gmail.com>
2020-04-24 10:58:05 -0700

    Updated design and added Type and Status

--- app.js
+++ app.js
@@ -1,236 +1,179 @@
-const form = document.querySelector('#anime-form');
-const animeList = document.querySelector('ul.collection');
-const animeInput = document.querySelector('#anime');
-const titleCount = document.querySelector('h3');
-const titleArr = [];
-
-// add load event listeners
-loadEventListeners();
-
-// add load event listeners
-function loadEventListeners(){
-  // DOM load event
-  document.addEventListener('DOMContentLoaded', getTitles);
-  // Add task event
-  form.addEventListener('submit', addAnime);
-  // Remove task event
-  animeList.addEventListener('click', removeAnime);
+class Anime {
+  constructor(title, type, status){
+    this.title = title;
+    this.type = type;
+    this.status = status;
+  }
 }
 
-function getTitles(){
-  let titles;
-  if(localStorage.getItem('titles') === null){
-    titles = [];
-  } else {
-    titles = JSON.parse(localStorage.getItem('titles'));
-    titleCount.innerText = 'Number of Titles: ' + titles.length;
+class UI {
+  addAnimeToList(anime){
+    const list = document.getElementById('anime-list');
+    const showList = document.querySelector('.container-2');
+    showList.style.display = 'block';
+
+    // Create tr element
+    const row = document.createElement('tr');
+  
+    // insert columns
+    row.innerHTML = `
+      <td>${anime.title}</td>
+      <td>${anime.type}</td>
+      <td>${anime.status}</td>
+      <td><a href="#" class="delete">X</a></td>
+    `;
+  
+    list.appendChild(row);
   }
 
-  titles.sort();
+  showAlert(message, className){
+    // create div
+    const div = document.createElement('div');
+    // add classes
+    div.className = `alert ${className}`;
+    // add text
+    div.appendChild(document.createTextNode(message));
+    // get parent
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#anime-form');
 
-  titles.forEach(function(title){
-    // Create li element
-    const li = document.createElement('li');
-    // Add class name
-    li.className = '.collection-item';
-    // Create text node and append
-    li.appendChild(document.createTextNode(title));
-    // Create link element
-    const link = document.createElement('a');
-    // Add class
-    link.className = 'delete-item secondary-content';
-    // Add icon HTML
-    link.innerHTML = '<i class="fa fa-remove"></i>';
-  
-    // Link styling
-    link.style.float = 'right';
-    link.style.color = 'gray';
-    // Link hover state: ON
-    link.addEventListener('mouseover', hoverOnLink);
-    function hoverOnLink(e){
-      link.style.color = 'coral';
-    }
-    // Link hover state: OFF
-    link.addEventListener('mouseout', hoverOffLink);
-    function hoverOffLink(e){
-      link.style.color = 'gray';
-    }
-  
-    // Append link to li
-    li.appendChild(link);
+    // Insert alert
+    container.insertBefore(div, form);
 
-    // insert styling (can't do on css sheet some reason)
-    li.style.padding = '.5rem';
-    li.style.margin = '.5rem 0';
-    // li.style.border = '1px solid black';
-    li.style.listStyle = 'none';
-    li.style.background = 'white';
-  
-    // Append li to ul
-    animeList.appendChild(li);
-  });
+    // timeout after 3s
+    setTimeout(function(){
+      document.querySelector('.alert').remove();
+    }, 3000);
+  }
+
+  deleteAnime(target){
+    if(target.className === 'delete'){
+      target.parentElement.parentElement.remove();
+      this.countAnime();
+    }
+  }
+
+  clearFields(){
+    document.getElementById('title').value = '';
+    document.getElementById('type').value = 'select';
+    document.getElementById('status').value = 'select';
+  }
+
+  countAnime(){
+    let titleCount = document.querySelector('.title-count');
+    titleCount.style.display = 'block';
+    titleCount.innerText = 'Number of Titles: ' + (document.querySelectorAll('tr').length - 1);
+  }
 }
 
-// Add anime event
-function addAnime(e){
-  if(animeInput.value === ''){
-    // alert('Add an anime or manga title');
-    showMessage('Please fill in the title field.', 'red')
-  } else {
-    // Create li element
-    const li = document.createElement('li');
-    // Add class name
-    li.className = '.collection-item';
-    // Create text node and append
-    li.appendChild(document.createTextNode(animeInput.value));
-    // Create link element
-    const link = document.createElement('a');
-    // Add class
-    link.className = 'delete-item secondary-content';
-    // Add icon HTML
-    link.innerHTML = '<i class="fa fa-remove"></i>';
-  
-    // Link styling
-    link.style.float = 'right';
-    link.style.color = 'gray';
-    // Link hover state: ON
-    link.addEventListener('mouseover', hoverOnLink);
-    function hoverOnLink(e){
-      link.style.color = 'coral';
+// Local Storage Class
+class Store {
+  static getAnimes(){
+    let animes;
+    if(localStorage.getItem('animes') === null){
+      animes = [];
+    } else {
+      animes = JSON.parse(localStorage.getItem('animes'));
     }
-    // Link hover state: OFF
-    link.addEventListener('mouseout', hoverOffLink);
-    function hoverOffLink(e){
-      link.style.color = 'gray';
-    }
-  
-    // Append link to li
-    li.appendChild(link);
 
-    // insert styling (can't do on css sheet some reason)
-    li.style.padding = '.5rem';
-    li.style.margin = '.5rem 0';
-    // li.style.border = '1px solid black';
-    li.style.listStyle = 'none';
-    li.style.background = 'white';
-  
-    // Append li to ul
-    animeList.appendChild(li);
-
-    showMessage(`"${animeInput.value}" was added to the list!`, 'green')
-
-
-    // let lis = document.querySelectorAll('li');
-    // lis.sort();
-
-    // adjust font size if name is too long (does not work)
-    // if(animeInput.value.length > 20){
-    //   li.style.fontSize = '1rem';
-    // }
-
-    // Store in Local Storage
-    storeTitlesInLocalStorage(animeInput.value);
-
-  
-    // Clear Input
-    animeInput.value = '';
-
-
-    titleCount.innerText = 'Number of Titles: ' + document.querySelectorAll('li').length;
-    
+    return animes;
   }
+
+  static displayAnimes(){
+    const animes = Store.getAnimes();
+
+    animes.forEach(function(anime){
+      const ui = new UI;
+      
+      // add title to UI
+      ui.addAnimeToList(anime);
+      ui.countAnime();
+    });
+  }
+
+  static addAnime(anime){
+    const animes = Store.getAnimes();
+
+    animes.push(anime);
+
+    localStorage.setItem('animes', JSON.stringify(animes));
+  }
+
+  static removeAnime(title){
+    const animes = Store.getAnimes();
+
+    animes.forEach(function(anime, index){
+      if(anime.title === title){
+        animes.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('animes', JSON.stringify(animes));
+  }
+}
+
+// DOM load event
+document.addEventListener('DOMContentLoaded', Store.displayAnimes);
+
+// Event Listener for Add
+document.getElementById('anime-form').addEventListener('submit', function(e){
+  // Get form values
+  const title = document.getElementById('title').value,
+        type = document.getElementById('type').value,
+        status = document.getElementById('status').value
+
+  // Instantiate anime
+  const anime = new Anime(title, type, status);
+
+  // Instantiate UI
+  const ui = new UI();
+
+  // Validate
+  if(title === ''|| type === 'select'|| status === 'select'){
+    //Error alert
+    ui.showAlert('Please fill in all fields', 'error');
+  } else {
+    // Add book to list
+    ui.addAnimeToList(anime);
+
+    // Add to LS
+    Store.addAnime(anime);
+
+    // Show success
+    ui.showAlert(`${title} was added!`, 'success');
+  
+    // Clear fields
+    ui.clearFields();
+
+    ui.countAnime();
+  }
+
 
   e.preventDefault();
-}
+})
 
+// Event Listener for Delete
+document.getElementById('anime-list').addEventListener('click', function(e){
+  // Instantiate UI
+  const ui = new UI();
 
+  // Delete Book
+  ui.deleteAnime(e.target);
 
-// Store Task
-function storeTitlesInLocalStorage(title){
-  let titles;
-  if(localStorage.getItem('titles') === null){
-    titles = [];
-  } else {
-    titles = JSON.parse(localStorage.getItem('titles'));
-  }
+  // Remove from LS
+  Store.removeAnime(e.target.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.textContent);
 
-  titles.push(title);
+  // Show message
+  ui.showAlert(`${e.target.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.innerText} was Removed!`, 'success');
 
-  localStorage.setItem('titles', JSON.stringify(titles));
-}
-
-// Remove anime event 
-function removeAnime(e){
-  const title = e.target.parentElement.parentElement.innerText;
-  if(e.target.parentElement.classList.contains('delete-item')){
-    if(confirm(`Remove "${title}" from the list?`)){
-      e.target.parentElement.parentElement.remove();
-      titleCount.innerText = 'Number of Titles: ' + document.querySelectorAll('li').length;
-
-      showMessage(`"${title}" was removed from the list`, 'blue')
-
-      // Remove from local storage
-      removeTitlesFromLocalStorage(e.target.parentElement.parentElement);
-    }
-  }
-}
-
-// Remove from Local storage
-function removeTitlesFromLocalStorage(titleItem){
-  let titles;
-  if(localStorage.getItem('titles') === null){
-    titles = [];
-  } else {
-    titles = JSON.parse(localStorage.getItem('titles'));
-  }
-
-  titles.forEach(function(title, index){
-    if(titleItem.textContent === title){
-      titles.splice(index, 1);
-    }
-  });
-
-  localStorage.setItem('titles', JSON.stringify(titles));
-}
-
-// showMessage Function
-function showMessage(message, color){
-  // create div
-  const messageDiv = document.createElement('div');
-
-  // get elements
-  const messageAlert = document.querySelector('.message');
-
-  // Add class
-  messageDiv.className = 'message-alert';
-
-  // Create text node and append to div
-  messageDiv.appendChild(document.createTextNode(message));
-
-  // Style div
-  messageDiv.style.textAlign = 'center';
-  messageDiv.style.color = color;
-  messageDiv.style.padding = '0';
-  messageDiv.style.margin = '0 0 1rem 0';
-
-  // Insert Message
-  messageAlert.appendChild(messageDiv);
-
-  // Clear message after 3 seconds
-  setTimeout(clearMessage, 3000);
-}
-
-// Clear Message
-function clearMessage(){
-  document.querySelector('.message-alert').remove();
-}
+  e.preventDefault();
+})
 
 // THINGS I WANT TO ADD:
 // 1) Sort button by alphabetical order and date entered
 // 2) add page numbers to longer lists
 // 3) Search Database (suggest ~3-5 titles as you type in letters)
-// 4) "Title was added" will show for 3s and then disappear
-// 5) Add a Favorite button/ star
+// 4) Add a Favorite button/ star
 
 // THINGS I'M NOT SURE OF ADDING
 // 1) change font size of longer titles inputted (text goes over the list if text string is too long)
@@ -243,3 +186,4 @@ function clearMessage(){
 // 2) Count list items and display change as one adds/removes from list
 // 3) Back to Top button 
 // 4) Messages pop up when no title entered, when title is added to list, and when title is removed from list
+// 5) "Title was added" will show for 3s and then disappear
